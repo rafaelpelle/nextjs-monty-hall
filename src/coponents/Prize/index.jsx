b367ffa753747f5469a909ae2dff5dc39dbafda9
Rafael Pelle <rafapelle@gmail.com>
2022-03-26 22:23:07 -0300

    implementing the Prize component

--- src/coponents/Prize/index.jsx
+++ src/coponents/Prize/index.jsx
@@ -1,5 +1,12 @@
 import styles from './Prize.module.css';
 
 export default function Prize() {
-  return <div className={styles.prize}>prize</div>;
+  return (
+    <div className={styles.prize}>
+      <div className={styles.cover} />
+      <div className={styles.body} />
+      <div className={styles.giftBow1} />
+      <div className={styles.giftBow2} />
+    </div>
+  );
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,8 @@
-import Door from '../coponents/Door';
 import Prize from '../coponents/Prize';
 
 export default function Home() {
   return (
     <div>
-      <Door />
       <Prize />
     </div>
   );
